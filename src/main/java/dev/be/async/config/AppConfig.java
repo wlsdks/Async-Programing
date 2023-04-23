828cfa223ef8e716d9422d6d9d6dc4cf2daafc77
jinan <ja.choi@sweetk.co.kr>
2023-04-23 13:04:32 +0900

    file init

---
+++ src/main/java/dev/be/async/config/AppConfig.java
@@ -0,0 +1,10 @@
+package dev.be.async.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+
+}
+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
