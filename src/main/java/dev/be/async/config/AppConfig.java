89c1a1f752ae740607f2fed9704530a733864483
jinan <ja.choi@sweetk.co.kr>
2023-04-23 13:34:19 +0900

    1. 쓰레드풀 설정

--- src/main/java/dev/be/async/config/AppConfig.java
+++ src/main/java/dev/be/async/config/AppConfig.java
@@ -1,10 +1,33 @@
 package dev.be.async.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AppConfig {
 
+    /**
+     * 쓰레드풀 설정
+     */
+    @Bean(name = "defaultTaskExecutor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor defaultTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(200);
+        executor.setMaxPoolSize(300);
+
+        return executor;
+    }
+
+    @Bean(name = "messagingTaskExecutor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor messagingTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(200);
+        executor.setMaxPoolSize(300);
+
+        return executor;
+    }
+
+
 
 }
-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
